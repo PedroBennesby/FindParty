5cc497b7d45024437491d12f6c02376ec304aa7c
Pedro Bennesby <Pedrobennesby@gmail.com>
2022-12-16 10:38:40 -0300

    feat: :tada: Project start and initial adjustments

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to FindParty!</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141414',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+  },
+});
